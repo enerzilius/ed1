cfd335993693ba2bc4f6063db40fa0aa100bbb01
enerzilius <efblouback@gmail.com>
2025-04-08 15:26:02 -0300

    + funções linkedlist

--- aulas/lista_encadeada/listaEnc.cpp
+++ aulas/lista_encadeada/listaEnc.cpp
@@ -14,12 +14,18 @@ LinkedList::~LinkedList() { };
 bool LinkedList::push_front(int key)  { 
     Node* node = new Node{key, nullptr};
     if(!node) return false;
+
     node->next = this->head;
     this->head = node; 
     return true;
 };
 
 bool LinkedList:: pop_front(){
+    if(!this->head) return false;
+
+    Node* node = this->head;
+    this->head = node->next;
+    delete node;
     return true;
 };
 
@@ -49,6 +55,8 @@ Node* LinkedList::find(int key) {
 };
 
 bool LinkedList::insert_after(int key, Node* pos) {
+    Node* node = new Node{key, pos->next};
+    pos->next = node;
     return true;
 };
 
@@ -56,8 +64,17 @@ bool LinkedList::remove_after(Node* pos){
     return true;
 };
 
-bool LinkedList::insert(int pos) {
+bool LinkedList::insert(int key, int pos) {
+    if (pos >= this->size()) return false;
 
+    Node* aux = this->head;
+    for (int i = 0; i < pos; i++)
+    {
+        aux = aux->next;
+    }
+    Node* novo = new Node{key, aux->next};
+    aux->next = novo;
+    return true;
 };
 
 bool LinkedList::remove(int pos){
@@ -69,3 +86,29 @@ bool LinkedList::remove(int key) {
 bool LinkedList::insert_sorted(int key) {
     return true;
 };
+
+int LinkedList::size() {
+    int n = 0;  
+    Node* node = this->head;
+    while (node)
+    {
+        node = node->next;
+        n++;
+    }
+    return n;
+    
+}
+
+bool LinkedList::equals(LinkedList* list) {
+    Node* temp = this->head;
+    Node* temp2 = list->head;
+    while(temp && temp2){
+        if(temp->key == temp2->key){
+            temp = temp->next;
+            temp2 = temp2->next;
+        }else{
+            return false;
+        }
+    }
+    return true;
+}
--- aulas/lista_encadeada/listaEnc.hpp
+++ aulas/lista_encadeada/listaEnc.hpp
@@ -36,8 +36,10 @@ public:
     Node* find(int key);
     bool insert_after(int key, Node* pos);
     bool remove_after(Node* pos);
-    bool insert(int pos);
+    bool insert(int key, int pos);
     bool remove(int pos);
     bool remove(int key);
     bool insert_sorted(int key);
+    int size();
+    bool equals(LinkedList* list);
 };
