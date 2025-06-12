79c8ca12943c8205b1c709d9e2ade17dd0e88bb9
enerzilius <efblouback@gmail.com>
2025-06-12 15:19:25 -0300

    recursao 2 finalizada

--- aulas/recursao2/recursao2.cpp
+++ aulas/recursao2/recursao2.cpp
@@ -35,9 +35,72 @@ int minLst(list<int>& lst, list<int>::iterator it){
     return (*it < minRest) ? *it : minRest;
 }
 
+bool find(vector<int>& vec, int n, int elem){
+    if (n >= vec.size()) return false;
+    if(vec[n] == elem) return true;
+    return find(vec, n+1, elem);
+}
+
+int count(vector<int>& vec, int n, int elem){
+    if (n >= vec.size()) return 0;
+    if(vec[n] == elem) return 1 + count(vec, n+1, elem);
+    return count(vec, n+1, elem);
+}
+
+void reverse(vector<int>& vec, int first, int last) {
+    if (first >= last) return;
+
+    int temp = vec[first];
+    vec[first] = vec[last];
+    vec[last] = temp;
+    
+    reverse(vec, first + 1, last - 1);
+}
+
+void reverseList(list<int>& lst, list<int>::iterator first, list<int>::iterator last){
+    if (first == last) return;
+
+    --last;
+    if (first == last) return;
+
+    int temp = *first;
+    *first = *last;
+    *last = temp;
+    
+    reverseList(lst, ++first, last);
+}
+
+bool palindrome(string& str, int first, int last) {
+    if (first >= last) {
+        return true;
+    }
+    if (str[first] != str[last]) {
+        return false;
+    }
+    return palindrome(str, first + 1, last - 1);
+}
+
+bool palindromeList(list<int>& lst, list<int>::iterator first, list<int>::iterator last) {
+    if (first == last) {
+        return true;
+    }
+    --last;
+    
+    if (first == last) {
+        return true;
+    }
+    
+    if (*first != *last) {
+        return false;
+    }
+    
+    ++first;
+    return palindromeList(lst, first, last);
+}
+
 
 int main(){
-    vector<int> vec = {4, 2, 3, 4};
+    vector<int> vec = {4, 2, 3, 4, 4};
     cout<<"Soma de um vetor: "<<sum(vec, 0)<<endl;
 
     list<int> lst = { 2, 3, 4, 2};
@@ -46,6 +109,33 @@ int main(){
     cout<<"Menor do vetor: "<<min(vec, 0)<<endl;
     it = lst.begin();
     cout<<"Menor da lista: "<<minLst(lst, it)<<endl;
+    cout<<(find(vec, 0, 10)?"Encontrado":"Não encontrado")<<endl;
+
+    cout<<"Número de vezes que 4 aparece: "<<count(vec, 0, 4)<<endl;
+
+    reverse(vec, 0, vec.size()-1);
+    cout<<"Vetor invertido: ";
+    for(int e : vec){
+        cout<<e<<" ";
+    }
+    cout<<endl;
+
+    it = lst.begin();
+    list<int>::iterator itLast = lst.end();
+    reverseList(lst, it, itLast);
+    cout<<"Lista invertida: ";
+    for(int e : lst){
+        cout<<e<<" ";
+    }   
+    cout<<endl;
+
+    string str = "arara";
+    cout<<(palindrome(str, 0, str.size()-1)?"Palindromo":"Não palindromo")<<endl;
+
+    list<int> palindromo = {1, 2, 2, 3};
+    it = palindromo.begin();
+    itLast = palindromo.end();
+    cout<<(palindromeList(palindromo, it, itLast)?"Palindromo":"Não palindromo")<<endl;
 
 
     return 0;
