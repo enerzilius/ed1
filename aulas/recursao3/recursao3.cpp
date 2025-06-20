6f0705bdb0effb8e4558083aad87eb1804486cda
enerzilius <efblouback@gmail.com>
2025-06-20 15:35:19 -0300

    socorro

--- aulas/recursao3/recursao3.cpp
+++ aulas/recursao3/recursao3.cpp
@@ -7,18 +7,55 @@
 
 using namespace std;
 
+float factorial(int n){
+    if(n <= 0) return 1;
+    return n*factorial(n-1);
+}
+
 float exponential(int n, int exp){
     if(exp == 0) return 1;
 
-    return n*exponential(n, exp-1);
+    if(exp > 0) return n*exponential(n, exp-1);
+    return 1/(n*exponential(n, exp*(-1)-1));
 }
 
 int MDC(int a, int b){
-    
+    if(b == 0) return a;
+    return MDC(b, a%b);
 }
 
+// void string_permutations(string str, int sub_len, int pos){
+//     // if(pos == str.length()-1) return;
+//     // for (int i = 0; i < str.length()-1; i++)
+//     // {
+//     //     for (int j = i; i < str.length()-1; i++)
+//     //     {
+//     //         swap(str[j], str[j+1]);
+//     //     }
+//     // }
+// }
+
+void backtracking(vector<int> vec) {
+    int offset = 0;
+    for (int i = 0; i < vec.size() - 1; i++) {
+        for (int j = i; j < vec.size() - offset; j++) {
+            for (int k = 0; k <= offset; k++) {
+                cout << vec[j + k] << " ";
+            }
+            cout << endl;
+        }
+        offset += 1;
+    }
+}
+
+
 int main(){
-    cout<<exponential(3,3)<<endl;
+    cout<<exponential(3,-3)<<endl;
+    cout<<MDC(16,8)<<endl;
+    string txt = "aaa";
+    // string_permutations(txt, 0, 0); 
+    vector<int> vec = {1, 2, 3};
+    backtracking(vec);
 
     return 0;
 }
